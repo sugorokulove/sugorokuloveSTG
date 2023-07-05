edde632dee983a5351a699be24d1ab44b0198026
sugorokulove <sugorokulove@gmail.com>
2023-07-05 11:36:44 +0900

    Add : 自機の移動操作

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -10,19 +10,22 @@ public class Player : MonoBehaviour
     private int state = 0;
     private bool noDamage = false;
 
+    private float speed = 0.0f;
+    private Vector3 position = Vector3.zero;
+
     // Start is called before the first frame update
     void Start()
     {
         state = 1;
         noDamage = true;
-        transform.localPosition = new Vector3(0.0f, -200.0f, 0.0f);
+
+        speed = 1.0f;
+        position = new Vector3(0.0f, -200.0f, 0.0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-        var position = transform.position;
-
         switch (state)
         {
             case 1:
@@ -34,9 +37,54 @@ public class Player : MonoBehaviour
                 }
                 flash.Flash();
                 break;
+            case 2:
+                PlayerControl();
+                break;
         }
 
         transform.position = position;
+    }
+
+    /// <summary>
+    /// 自機操作
+    /// </summary>
+    void PlayerControl()
+    {
+        // キーボード操作
+        if (Input.GetKey(KeyCode.D))
+        {
+            position.x += speed;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            position.x -= speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            position.y += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            position.y -= speed;
+        }
+
+        // 画面内移動範囲
+        if (position.y <= -168.0f)
+        {
+            position.y = -168.0f;
+        }
+        if (position.y >= 168.0f)
+        {
+            position.y = 168.0f;
+        }
+        if (position.x <= -224.0f)
+        {
+            position.x = -224.0f;
+        }
+        if (position.x >= 224.0f)
+        {
+            position.x = 224.0f;
+        }
     }
 
     void Damage()
