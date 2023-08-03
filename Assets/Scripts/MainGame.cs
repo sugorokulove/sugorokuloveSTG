58fd8cd9bf9bce0638b576cf506447edac4837f1
sugorokulove <sugorokulove@gmail.com>
2023-08-03 17:32:39 +0900

    Add : 自機の残機作成

--- Assets/Scripts/MainGame.cs
+++ Assets/Scripts/MainGame.cs
@@ -1,11 +1,30 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Linq;
+using UnityEngine;
 
 public class MainGame : MonoBehaviour
 {
+    [SerializeField] private int m_stock = 3;                               // 残機数指定
+
+    private GameObject[] m_stockList = null;                                // 残機管理(GameObject)
+    public int Stock => m_stockList.Where(obj => obj != null).Count();      // 残機数
+
+    void Awake()
+    {
+        m_stockList = new GameObject[m_stock];
+        Array.Fill(m_stockList, null);
+    }
+
     void Start()
     {
+        // 指定数残機追加
+        for (int i = 0; i < m_stock; i++)
+        {
+            AddStock();
+        }
+
         // ゲーム開始時の自機の生成
-        GeneratePlayer();
+        ReStart();
     }
 
     /// <summary>
@@ -13,7 +32,27 @@ public class MainGame : MonoBehaviour
     /// </summary>
     public void ReStart()
     {
-        Invoke(nameof(GeneratePlayer), 1.0f);
+        if (Stock > 0)
+        {
+            Invoke(nameof(GeneratePlayer), 1.0f);
+        }
+    }
+
+    /// <summary>
+    /// 残機追加
+    /// </summary>
+    private void AddStock()
+    {
+        m_stockList[Stock] = GenerateStock();
+    }
+
+    /// <summary>
+    /// 残機削除
+    /// </summary>
+    private void RemoveStock()
+    {
+        Destroy(m_stockList[Stock - 1]);
+        m_stockList[Stock - 1] = null;
     }
 
     /// <summary>
@@ -24,5 +63,20 @@ public class MainGame : MonoBehaviour
         var prefab = Resources.Load<GameObject>("Prefabs/Plane/Player");
         var player = Instantiate(prefab);
         player.GetComponent<Player>().Initialize();
+
+        RemoveStock();
+    }
+
+    /// <summary>
+    /// 残機の生成
+    /// </summary>
+    GameObject GenerateStock()
+    {
+        var prefab = Resources.Load<GameObject>("Prefabs/Plane/Stock");
+        var stock = Instantiate(prefab);
+        stock.transform.position = new Vector3(
+            -GameInfo.Instance.ScreenBound.x + Stock * 16 + 16,
+            -GameInfo.Instance.ScreenBound.y + 16);
+        return stock;
     }
 }
