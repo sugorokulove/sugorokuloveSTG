7fa1450a69056ac66de79c3fbe393c6baf3023a2
sugorokulove <sugorokulove@gmail.com>
2023-11-23 13:59:40 +0900

    Add : パーティクルの利用

--- Assets/Scripts/Explosion.cs
+++ Assets/Scripts/Explosion.cs
@@ -1,14 +1,21 @@
-﻿using UnityEngine;
+﻿using System.Collections;
+using UnityEngine;
 
 public class Explosion : MonoBehaviour
 {
+    private ParticleSystem m_ps = null;
+
     /// <summary>
     /// 初期化
     /// </summary>
     /// <param name="position">生成位置</param>
     public void Initialize(Vector3 position)
     {
+        m_ps = GetComponent<ParticleSystem>();
         transform.position = position;
+
+        // パーティクル終了を検知する
+        StartCoroutine(ParticleWorking());
     }
 
     /// <summary>
@@ -18,4 +25,23 @@ public class Explosion : MonoBehaviour
     {
         ObjectPoolManager.Instance.Return(this);
     }
+
+    /// <summary>
+    /// パーティクル終了時にオブジェクトを返却する
+    /// </summary>
+    /// <returns></returns>
+    IEnumerator ParticleWorking()
+    {
+        yield return new WaitWhile(() => m_ps.IsAlive(true));
+        ObjectPoolManager.Instance.Return(this);
+    }
+#if false
+    /// <summary>
+    /// パーティクル終了時に自動で呼ばれる関数
+    /// </summary>
+    public void OnParticleSystemStopped()
+    {
+        ObjectPoolManager.Instance.Return(this);
+    }
+#endif
 }
